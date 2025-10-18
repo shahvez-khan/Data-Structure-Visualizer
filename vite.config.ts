c0ca44529c3f868f9997a803f6b57dd3e0e4d95d
shahvez-khan <shahvezrahmankhan786@gmail.com>
2025-10-18 18:50:54 +0530

    vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   // ADD this line to explicitly set the base path to the root
-  base: '/data-structure-visualizer/',
+  base: '/',
   plugins: [react()],
 });
